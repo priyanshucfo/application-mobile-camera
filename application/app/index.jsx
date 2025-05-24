4ba508e0c3217b47d49f960c137bb49fc264e6d4
CoderFightOfficial <coderfightofficial@gmail.com>
2025-05-24 19:30:55 +0530

    first commit

---
+++ application/app/index.jsx
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    color: "blue"
+  }
+});
